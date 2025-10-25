1232b9042b9f816265a5723c25c456c9ba5917d2
Travis Biehle <travissbiehle@gmail.com>
2025-10-24 22:23:11 -0700

    fix: header formatting, global header height variable

--- fsc_app/app/components/header.tsx
+++ fsc_app/app/components/header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="sticky top-0 flex w-full h-15 z-10 justify-center p-2 bg-tan-20 rounded-b-2xl">
+    <div className="sticky top-0 flex w-full h-(--header-height) z-10 justify-center p-2 bg-tan-20 rounded-b-2xl">
       <div className="relative flex w-1/3 max-w-xs">
         <Image
           src={"/FSC_Logo_Big.svg"}
--- fsc_app/app/layout.tsx
+++ fsc_app/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased bg-cloud`}>
-        <Header />
+        <Header/>
         {children}
       </body>
     </html>
