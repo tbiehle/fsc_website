e00922e0f28201d43f15a2fdc76105542bd8ac6d
Travis Biehle <travissbiehle@gmail.com>
2025-10-24 22:24:59 -0700

    progress on gear page and Add Gear form.

--- fsc_app/src/lib/firebase.ts
+++ fsc_app/src/lib/firebase.ts
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app, "fsc-db");
-const storage = getFirestore(app)
+const storage = getStorage(app);
 
 export { app, db, storage }
